b134816a6bf95338e52967530d9d0ffe8acc4fcf
Jsman <VuReact@qq.com>
2020-11-22 16:57:20 +0800

    finish home

--- src/page/index/index.tsx
+++ src/page/index/index.tsx
@@ -2,8 +2,10 @@ import React, { FC } from "react";
 import Search from "../../components/search/search";
 import Top from "./components/top/top";
 import CodeCoffeeImg from "../../assets/img/codeCoffee.jpg";
-
+import Mac from "../../assets/img/Mac.jpg";
+import ManCodeing from "../../assets/img/ManCodeing.jpg";
 import "./index.scss";
+
 type Props = {};
 
 const Index: FC<Props> = () => {
@@ -17,11 +19,11 @@ const Index: FC<Props> = () => {
             <h2>每一行都妙不可言</h2>
           </div>
           <div data-aos="zoom-in-left" className="Show reverse">
-            <img src={CodeCoffeeImg} alt="" />
+            <img src={Mac} alt="" />
             <h2>唯一的不同，是处处都不同</h2>
           </div>
           <div data-aos="zoom-out-up" className="Show">
-            <img src={CodeCoffeeImg} alt="" />
+            <img src={ManCodeing} alt="" />
             <h2>这是我独享的Moment</h2>
           </div>
         </div>
