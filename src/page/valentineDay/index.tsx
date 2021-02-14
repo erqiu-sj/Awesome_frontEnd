fca457187fd47386a2e28589458ed21749b9a0b7
Jsman <VuReact@qq.com>
2021-02-14 13:27:24 +0800

    feat: add love

--- src/page/valentineDay/index.tsx
+++ src/page/valentineDay/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: 邱狮杰
  * @Date: 2021-02-14 12:51:13
- * @LastEditTime: 2021-02-14 12:53:01
+ * @LastEditTime: 2021-02-14 13:25:41
  * @FilePath: /Awesome_frontEnd-main/src/page/valentineDay/index.tsx
  * @Description: 情人节限定
  *
@@ -13,6 +13,7 @@ type Props = {};
 const ValentineDay: FC<Props> = () => {
   return (
     <div id="lover">
+      <h1>老婆情人节快乐</h1>
       <ul>
         <li></li>
         <li></li>
